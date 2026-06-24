c423a7d7fa9bf3d4794f3573353939d20a5f3f51
Shalehin Ahmed Ornob <saornob2001@gmail.com>
2026-06-24 22:29:48 +0600

    Test

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient, jwtClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000" || process.env.BETTER_AUTH_URL,
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [
     adminClient(),
     jwtClient()
